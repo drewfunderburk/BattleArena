3cf55c2d71085787252bb48b55a7a2db66dfe50e
Judeous <judeous@tutanota.com>
2021-01-13 10:00:32 -0600

    m_fireDelay and empty fire
    Added m_fireDelay and put into constructors
    Added empty fire function

--- raygame/SpaceActor.cpp
+++ raygame/SpaceActor.cpp
@@ -1,21 +1,28 @@
 #include "SpaceActor.h"
 
-SpaceActor::SpaceActor(float health, float x, float y, float collisionRadius, char icon, float maxSpeed) : Actor(x, y, collisionRadius, icon, maxSpeed)
+SpaceActor::SpaceActor(float health, float coolDown, float x, float y, float collisionRadius, char icon, float maxSpeed) : Actor(x, y, collisionRadius, icon, maxSpeed)
 {
     m_health = health;
+    m_fireDelay = coolDown;
 }
 
-SpaceActor::SpaceActor(float health, float x, float y, float collisionRadius, Sprite* sprite, float maxSpeed) : Actor(x, y, collisionRadius, sprite, maxSpeed)
+SpaceActor::SpaceActor(float health, float coolDown, float x, float y, float collisionRadius, Sprite* sprite, float maxSpeed) : Actor(x, y, collisionRadius, sprite, maxSpeed)
 {
     m_health = health;
+    m_fireDelay = coolDown;
 }
 
-SpaceActor::SpaceActor(float health, float x, float y, float collisionRadius, const char* spriteFilePath, float maxSpeed) : Actor(x, y, collisionRadius, spriteFilePath, maxSpeed)
+SpaceActor::SpaceActor(float health, float coolDown, float x, float y, float collisionRadius, const char* spriteFilePath, float maxSpeed) : Actor(x, y, collisionRadius, spriteFilePath, maxSpeed)
 {
     m_health = health;
+    m_fireDelay = coolDown;
 }
 
 void SpaceActor::takeDamage(float damageVal)
 {
 	m_health -= damageVal;
+}
+
+void SpaceActor::fire()
+{
 }
