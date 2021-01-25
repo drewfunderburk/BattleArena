4610926d49d57ea153eb7d55ccb4b693dcaeceda
Drew Funderburk <kevinfunderburkbusiness@gmail.com>
2021-01-25 12:16:15 -0600

    Fixed sprite scaling

--- raygame/Sprite.cpp
+++ raygame/Sprite.cpp
@@ -46,16 +46,14 @@ void Sprite::draw(MathLibrary::Matrix3 transform)
     MathLibrary::Vector2 pos = MathLibrary::Vector2(transform.m13, transform.m23);
     MathLibrary::Vector2 forward = MathLibrary::Vector2(transform.m11, transform.m21);
     MathLibrary::Vector2 up = MathLibrary::Vector2(transform.m12, transform.m22);
-    pos = pos - (forward.getNormalized() * getWidth() / 2);
-    pos = pos - (up.getNormalized() * getHeight() / 2);
+    pos = pos - (forward.getNormalized() * (getWidth() / 2.0) * m_scale);
+    pos = pos - (up.getNormalized() * (getHeight() / 2.0) * m_scale);
 
     //Find the transform rotation in radians 
     float rotation = atan2(transform.m21, transform.m11);
-    RAYLIB_H::Vector2 rayPos = { pos.x*32, pos.y*32 };
+    RAYLIB_H::Vector2 rayPos = { pos.x, pos.y };
 
     //Draw the sprite
     RAYLIB_H::DrawTextureEx(*m_texture, rayPos,
-        (float)(rotation * 180.0f / PI), 32, WHITE);
-
-    DrawRectangleLines(rayPos.x, rayPos.y, getWidth() * 32, getHeight() * 32, RED);
+        (float)(rotation * 180.0f / PI), m_scale, WHITE);
 }
